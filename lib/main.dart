96efc986005d2bd38e4aa1501ae70fa7386bb348
ehgus0526 <ehgus0526@naver.com>
2025-05-01 17:57:12 +0900

    feat: connect book info url

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:book_store/book.dart';
 import 'package:book_store/book_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(
@@ -119,6 +120,11 @@ class HomePage extends StatelessWidget {
                         ),
                         title: Text(book.title),
                         subtitle: Text(book.subtitle),
+                        onTap: () {
+                          // 클릭시 previewLink 띄우기
+                          Uri uri = Uri.parse(book.previewLink);
+                          launchUrl(uri);
+                        },
                       );
                     },
                   ),
