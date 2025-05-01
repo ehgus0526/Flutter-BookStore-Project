93039d0ced938c9a7f93e960bd411a0b8ca19765
ehgus0526 <ehgus0526@naver.com>
2025-05-01 17:10:19 +0900

    feat: link google books api

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,17 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
-  // 검색어를 가져올 수 있도록 TextField와 연결
+  /// 검색어를 가져올 수 있도록 TextField와 연결해 줍니다.
   final TextEditingController searchController = TextEditingController();
+
+  /// 검색 함수
+  /// 엔터를 누르거나 돋보기 아이콘을 누를 때 호출
+  void search(BookService bookService) {
+    String keyword = searchController.text;
+    if (keyword.isNotEmpty) {
+      bookService.getBookList(keyword);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,18 +41,28 @@ class HomePage extends StatelessWidget {
       builder: (context, bookService, child) {
         return Scaffold(
           appBar: AppBar(
+            backgroundColor: Colors.white,
             centerTitle: false,
             title: Text(
               "Book Store",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
             actions: [
               Container(
                 alignment: Alignment.bottomCenter,
                 padding: const EdgeInsets.only(right: 12),
-                child: Text("total ${bookService.bookList.length}"),
+                child: Text(
+                  "total ${bookService.bookList.length}",
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
               ),
             ],
+
+            /// AppBar의 Bottom은 항상 PreferredSize 위젯으로 시작해야합니다.
             bottom: PreferredSize(
               preferredSize: Size(double.infinity, 72),
               child: Padding(
@@ -51,16 +70,25 @@ class HomePage extends StatelessWidget {
                 child: TextField(
                   controller: searchController,
                   decoration: InputDecoration(
+                    hintText: "원하시는 책을 검색해주세요.",
+                    // 테두리
                     border: OutlineInputBorder(
                       borderSide: BorderSide(color: Colors.black),
                     ),
-                    hintText: "원하시는 책을 검색해주세요",
+
+                    /// 돋보기 아이콘
                     suffixIcon: IconButton(
-                      onPressed: () {},
                       icon: Icon(Icons.search),
+                      onPressed: () {
+                        // 돋보기 아이콘 클릭
+                        search(bookService);
+                      },
                     ),
                   ),
-                  onSubmitted: (v) {},
+                  onSubmitted: (v) {
+                    // 엔터를 누르는 경우
+                    search(bookService);
+                  },
                 ),
               ),
             ),
