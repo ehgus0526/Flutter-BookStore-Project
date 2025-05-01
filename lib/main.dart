f4eeb0be1d29de47e64f1ddd5f7f4f6cbf67283b
ehgus0526 <ehgus0526@naver.com>
2025-05-01 16:55:30 +0900

    feat: add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:book_store/book_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => BookService())],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,8 +23,50 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
+  // 검색어를 가져올 수 있도록 TextField와 연결
+  final TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Consumer<BookService>(
+      builder: (context, bookService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: false,
+            title: Text(
+              "Book Store",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            actions: [
+              Container(
+                alignment: Alignment.bottomCenter,
+                padding: const EdgeInsets.only(right: 12),
+                child: Text("total ${bookService.bookList.length}"),
+              ),
+            ],
+            bottom: PreferredSize(
+              preferredSize: Size(double.infinity, 72),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    hintText: "원하시는 책을 검색해주세요",
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.search),
+                    ),
+                  ),
+                  onSubmitted: (v) {},
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
